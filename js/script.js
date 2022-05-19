39f2999696e717f4e5ff2fc4c5558547078170ab
Abdul Razak Shaikh <89abdul@gmail.com>
2022-05-19 16:29:15 +0530

    Add files via upload
    chnages

--- js/script.js
+++ js/script.js
@@ -26,6 +26,29 @@ $(function() {
             return !1
         })
     });
-    
+
+    $(document).ready(function() {
+        var scrollLink = $('.scroll');
+        scrollLink.click(function(e) {
+            e.preventDefault();
+            $('body,html').animate({
+                scrollTop: $(this.hash).offset().top - 90
+            }, 1000)
+        })
+    });
+
+    // var offset = 90;
+
+    // $('.navbar-nav .nav-link').click(function(event) {
+    //     event.preventDefault();
+    //     $($(this).attr('href'))[0].scrollIntoView();
+    //     scrollBy(0, -offset);
+    // });
+    // $('.smooth-scroll').on('click', function() {
+    //     event.preventDefault();
+    //     var sectionTo = $(this).attr('href');
+    //     $('html, body').stop().animate({
+    //       scrollTop: $(sectionTo).offset().top}, 1500, 'easeInOutExpo');
+    // });
 
 });
